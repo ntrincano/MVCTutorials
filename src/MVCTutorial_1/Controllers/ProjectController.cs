9e6b9997e576679d3cf75c6cd90427a2db15f38b
ntrincano <ntrincano@gmail.com>
2016-04-27 15:18:45 +0200

    Add project controller

---
+++ src/MVCTutorial_1/Controllers/ProjectController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCTutorial_1.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "Salut !";
+        }
+
+        public string HelloWorld(string Id)
+        {
+            return HtmlEncoder.Default.HtmlEncode(string.Format("Salut {0} !", Id));
+        }
+    }
+}
